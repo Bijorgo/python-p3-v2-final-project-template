78a4b2b2ab888a5ab76bd736d7d2573348c4b5d6
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-01-17 14:53:43 -0700

    Initial playlist attributes

--- lib/models/playlist.py
+++ lib/models/playlist.py
@@ -1,3 +1,23 @@
 class Playlist:
-    def __init__(self):
-        pass
+    def __init__(self, name, description):
+        self._name = name
+        self._description = description
+
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def description(self):
+        return self._description
+    
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str) and len(name) > 0:
+            raise ValueError("Name must be a string of 1 or more characters")
+        self._name = name
+
+    @description.setter
+    def description(self, description):
+        if not isinstance(description, str) and ( 0 <= len(description) <= 50):
+            raise ValueError("Description must be a string between 0 and 50 characters")
