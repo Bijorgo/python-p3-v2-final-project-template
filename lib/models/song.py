e24769abc7001d4371fa098760d3c70c56a74c17
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-01-17 14:56:24 -0700

    add validations to song attributes

--- lib/models/song.py
+++ lib/models/song.py
@@ -1,5 +1,5 @@
 class Song:
-    def __init__(self, title, artist, genre, duration):
+    def __init__(self, title, artist, genre, duration=None):
         self._title = title
         self._artist = artist
         self._genre = genre
@@ -23,19 +23,19 @@ class Song:
     
     @title.setter
     def title(self, title):
-        if not isinstance(title, str):
-            raise ValueError("Title must be a string")
+        if not isinstance(title, str) and len(title) > 0:
+            raise ValueError("Title must be a string of one or more characters")
         self._title = title
 
     @artist.setter
     def artist(self, artist):
-        if not isinstance(artist, str):
-            raise ValueError("Artist must be a string")
+        if not isinstance(artist, str) and len(artist) > 0:
+            raise ValueError("Artist must be a string one or more characters")
         
     @genre.setter
     def genre(self, genre):
-        if not isinstance(genre, str):
-            raise ValueError("Genre must be a string")
+        if not isinstance(genre, str) and len(genre) > 0:
+            raise ValueError("Genre must be a string one or more characters")
         
     @duration.setter
     def duration(self, duration):
