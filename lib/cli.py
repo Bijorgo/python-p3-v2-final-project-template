38f253510208666fc428dca807e0621fc0c0be18
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-01-20 19:45:34 -0700

    Update cli menu

--- lib/cli.py
+++ lib/cli.py
@@ -28,54 +28,67 @@ def main():
     while True:
         menu()
         choice = input("> ")
-        if choice == "0":
+        if choice == "EXIT":
             exit_program()
         elif choice == "1":
             create_new_playlist()
-        elif choice == "update playlist":
-            update_playlist_info
         elif choice == "2":
-            delete_playlist()
+            update_playlist_info
         elif choice == "3":
-            display_all_playlists()
+            display_all_playlists() 
         elif choice == "4":
             find_playlist_by_name()
         elif choice == "5":
-            create_new_song()
-        elif choice == "update song":
-            update_song_info()
-        elif choice == "6":
-            delete_song()
-        elif choice == "7":
-            display_all_songs()
-        elif choice == "8":
-            find_song_by_title()
-        elif choice == "9":
             adding_song_to_playlist()
+        elif choice == "6":
+            view_songs_in_playlist()   
+        elif choice == "7":
+            create_new_song()
+        elif choice == "8":
+            update_song_info()     
+        elif choice == "9":
+            display_all_songs()
         elif choice == "10":
-            view_songs_in_playlist()
+            find_song_by_title()               
         elif choice == "11":
+            delete_playlist()
+        elif choice == "12":
+            delete_song()
+        elif choice == "13":
             clear_all_relationships()
         else:
             print("Invalid choice")
+        
+        
+        
+        
+        
+        
 
 
 def menu():
     print("Please select an option:")
-    print("0. Exit the program")
+    print("~~~~PLAYLIST OPTIONS:~~~~")
+    #print("~~~~~~~~~~~~~~~~~~~~~")
     print("1. Create New Playlist")
-    print("2. Delete Playlist")
+    print("2. Update playlist")
     print("3. Display all Playlists")
     print("4. Find A Playlist")
-    print("5. Add A Song")
-    print("6. Delete A Song")
-    print("7. Display All Songs")
-    print("8. Find A Song")
-    print("9. Add Song to Playlist")
-    print("10. View Songs In Playlist")
-    print("11. Clear All Songs In All Playlists")
-    print("update playlist")
-    print("update song")
+    print("5. Add Song to Playlist")
+    print("6. View Songs In Playlist")
+    print("~~~~SONG OPTIONS:~~~~")
+    #print("~~~~~~~~~~~~~~~~~~~~~")
+    print("7. Create A Song")
+    print("8. Update song")
+    print("9. Display All Songs")
+    print("10. Find A Song")
+    print("~~~~DELETE OPTIONS~~~~")
+    #print("~~~~~~~~~~~~~~~~~~~~~")
+    print("11. Delete Playlist")
+    print("12. Delete A Song")
+    print("13. Clear All Songs In All Playlists")
+    print("~~~~~~~~~~~~~~~~~~~~~")
+    print("EXIT to Exit the program")
 
 
 if __name__ == "__main__":
